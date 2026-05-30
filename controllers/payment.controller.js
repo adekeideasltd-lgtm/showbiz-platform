6f980adb48d9cfd05124072477713f4e583d2fd2
Adeke Ideas Limited <agbatadennis@gmail.com>
2026-05-30 06:52:06 +0100

    Fix: payment payer/owner alias mismatch

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -19,7 +19,7 @@ const initiatePayment = async (req, res) => {
     if (!booking_id) { await t.rollback(); return res.status(400).json({ status: 'error', message: 'booking_id required.' }); }
 
     const booking = await db.Booking.findByPk(booking_id, {
-      include: [{ model: db.User, as: 'payer', attributes: ['id','email','first_name','last_name'] }],
+      include: [{ model: db.User, as: 'owner', attributes: ['id','email','first_name','last_name'] }],
       transaction: t,
     });
 
